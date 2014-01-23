1a7394f7fb8161e87486462a2290952089b5f74b
Digvijay <digvijag@thoughtworks.com>
2014-01-23 09:55:30 +0530

    Build file added and tested using ant.

--- src/WrongPerson.java
+++ src/WrongPerson.java
@@ -1,25 +1,9 @@
 public class WrongPerson {
-    private String name;
-    private int age;
+    public String name;
+    public int age;
 
     public WrongPerson(String name, int age) {
         this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
         this.age = age;
     }
 
